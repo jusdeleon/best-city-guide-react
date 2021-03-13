7a088cb6aafba5f622f3b59c930523d1215f483d
Justin De Leon <jusmdeleon@gmail.com>
2021-03-13 22:03:07 +0800

    Remove span

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,9 +2,7 @@ function Header() {
   return (
     <div className="container mx-auto p-5">
       <div className="text-center md:flex md:justify-between md:items-center">
-        <h1 className="text-xl text-indigo-900 mb-5 md:mb-0">
-          <span>Best City Guide</span>
-        </h1>
+        <h1 className="text-xl text-indigo-900 mb-5 md:mb-0 cursor-pointer">Best City Guide</h1>
         <div className="flex flex-col md:flex-row">
           <span className="py-2 md:py-0 md:mx-5 text-teal-500 hover:text-indigo-900 uppercase tracking-wide cursor-pointer">
             Ice Cream
