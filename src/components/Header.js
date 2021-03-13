c194f45a507b47a55ecbb719eb172353460f5e3f
Justin De Leon <jusmdeleon@gmail.com>
2021-03-13 22:02:09 +0800

    Fix warnings

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,21 +3,21 @@ function Header() {
     <div className="container mx-auto p-5">
       <div className="text-center md:flex md:justify-between md:items-center">
         <h1 className="text-xl text-indigo-900 mb-5 md:mb-0">
-          <a href="#">Best City Guide</a>
+          <span>Best City Guide</span>
         </h1>
         <div className="flex flex-col md:flex-row">
-          <a href="#" className="py-2 md:py-0 md:mx-5 text-teal-500 hover:text-indigo-900 uppercase tracking-wide">
+          <span className="py-2 md:py-0 md:mx-5 text-teal-500 hover:text-indigo-900 uppercase tracking-wide cursor-pointer">
             Ice Cream
-          </a>
-          <a href="#" className="py-2 md:py-0 md:mx-5 text-teal-500 hover:text-indigo-900 uppercase tracking-wide">
+          </span>
+          <span className="py-2 md:py-0 md:mx-5 text-teal-500 hover:text-indigo-900 uppercase tracking-wide cursor-pointer">
             Donuts
-          </a>
-          <a href="#" className="py-2 md:py-0 md:mx-5 text-teal-500 hover:text-indigo-900 uppercase tracking-wide">
+          </span>
+          <span className="py-2 md:py-0 md:mx-5 text-teal-500 hover:text-indigo-900 uppercase tracking-wide cursor-pointer">
             Tea
-          </a>
-          <a href="#" className="py-2 md:py-0 md:mx-5 text-teal-500 hover:text-indigo-900 uppercase tracking-wide">
+          </span>
+          <span className="py-2 md:py-0 md:mx-5 text-teal-500 hover:text-indigo-900 uppercase tracking-wide cursor-pointer">
             Coffee
-          </a>
+          </span>
         </div>
       </div>
     </div>
